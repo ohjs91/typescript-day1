ba1cf98cb52378cd4240454d01513e15f3652ced
진석 <ozjinseok@gmail.com>
2026-01-02 13:39:44 +0900

    기본 타입 (string, number, boolean, null, any) 실습

---
+++ index.js
@@ -0,0 +1,23 @@
+function hello(name) {
+    console.log("Hello ".concat(name));
+}
+hello("Typescript");
+var myString;
+myString = "Hello! Typescript";
+console.log("String", myString);
+var myNumber;
+myNumber = 123;
+console.log("Number", myNumber);
+var myBoolean;
+myBoolean = true;
+console.log("Boolean", myBoolean);
+var myNull;
+myNull = null;
+console.log("Null", myNull);
+var myAny;
+myAny = "any는 아무거나 모든 타입";
+console.log("Any string", myAny);
+myAny = 123;
+console.log("Any Number", myAny);
+myAny = true;
+console.log("Any Boolean", myAny);
